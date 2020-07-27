0688a24869df710f99654b3e4ef0326dfa216132
Nemanja Kuzmanovic <kuzmaneca@gmail.com>
2020-07-27 09:25:07 +0200

    update dependencies, simplify middleware

--- src/middleware/setBodyFieldFromAuthUser.ts
+++ src/middleware/setBodyFieldFromAuthUser.ts
@@ -1,10 +1,9 @@
-// Used as a middleware to set a body field equal
-// to a user id pulled from the jwt token
-
 import { RequestHandler } from 'express';
 
+// Used as a middleware to set a body field equal
+// to a user id pulled from the jwt token
 // e.g used to connect a current logged in user to a created resource
-const setBodyFieldFromAuthUser = (fieldName: string = ''): RequestHandler => (req, res, next) => {
+const setBodyFieldFromAuthUser = (fieldName: string): RequestHandler => (req, res, next) => {
   if (!req.body[fieldName]) req.body[fieldName] = req.user.id;
   next();
 };
--- src/middleware/setFindFilterFromRequest.ts
+++ src/middleware/setFindFilterFromRequest.ts
@@ -9,10 +9,11 @@ enum FILTER_TYPE {
 // Use FILTER_TYPE.REQ_PARAMS when getting results connected to the resource id specified as a path variable
 // Use FILTER_TYPE.REQ_USER for filtering results connected to the currently logged in user
 // it sets the filter on req.dbFilter field which is later accessed in the handlerFactory.getAll function
+// key defaults [fieldToSet]Id
 const setFindFilterFromRequest = (
-  fieldToSet: string = '',
+  fieldToSet: string,
   reqField: FILTER_TYPE = FILTER_TYPE.REQ_PARAMS,
-  key: string = ''
+  key: string = `${fieldToSet}Id`
 ): RequestHandler => (req, res, next) => {
   // @ts-ignore
   const value = req[reqField][key];
